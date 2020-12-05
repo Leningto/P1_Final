b14cedc5ba2ee77620c5949da8b04ebe1e763722
Fernan2601 <56654113+Fernan2601@users.noreply.github.com>
2020-12-05 01:04:56 -0400

    Arreglo visual

--- P1_final/src/visiual/AgregarCurriculum.java
+++ P1_final/src/visiual/AgregarCurriculum.java
@@ -11,10 +11,14 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JCheckBox;
 
 public class AgregarCurriculum extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -55,13 +59,38 @@ public class AgregarCurriculum extends JFrame {
 		lblNuevoCurriculum.setBounds(142, 11, 149, 31);
 		panel.add(lblNuevoCurriculum);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(28, 43, 46, 14);
+		JLabel lblNewLabel = new JLabel("Genero");
+		lblNewLabel.setBounds(10, 52, 74, 14);
 		panel.add(lblNewLabel);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Femenino", "Masculino"}));
-		comboBox.setBounds(10, 68, 92, 20);
+		comboBox.setBounds(10, 68, 103, 20);
 		panel.add(comboBox);
+		
+		JLabel lblIdiomasDominados = new JLabel("Idiomas dominados");
+		lblIdiomasDominados.setBounds(10, 99, 116, 14);
+		panel.add(lblIdiomasDominados);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(10, 124, 103, 132);
+		panel.add(textArea);
+		
+		JLabel lblSueldoIdealrd = new JLabel("Sueldo ideal (RD$)");
+		lblSueldoIdealrd.setBounds(10, 267, 116, 14);
+		panel.add(lblSueldoIdealrd);
+		
+		textField = new JTextField();
+		textField.setBounds(5, 282, 108, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lbltieneLicenciaDe = new JLabel("\u00BFTiene licencia de conducir?");
+		lbltieneLicenciaDe.setBounds(10, 313, 176, 14);
+		panel.add(lbltieneLicenciaDe);
+		
+		JCheckBox chckbxNewCheckBox = new JCheckBox("");
+		chckbxNewCheckBox.setBounds(92, 334, 21, 23);
+		panel.add(chckbxNewCheckBox);
 	}
 }
